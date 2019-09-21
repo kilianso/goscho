10dc28e7e8d65a245393886559435a6b0df95bdc
Unknown <hello@kilianso.com>
2019-09-21 15:52:32 +0200

    change to ASC order, add content

--- wp-content/themes/goscho/functions.php
+++ wp-content/themes/goscho/functions.php
@@ -244,3 +244,19 @@ function set_post_content( $entry, $form ) {
 
 }
 add_action( 'gform_after_submission', 'set_post_content', 10, 2 );
+
+//function to modify default WordPress query
+function wpb_custom_query( $query ) {
+ 
+	// Make sure we only modify the main query on the homepage  
+		if( $query->is_main_query() && ! is_admin() && $query->is_home() ) {
+	 
+			// Set parameters to modify the query
+			$query->set( 'orderby', 'date' );
+			$query->set( 'order', 'ASC' );
+			$query->set( 'suppress_filters', 'true' );
+		}
+	}
+	 
+// Hook our custom query function to the pre_get_posts 
+add_action( 'pre_get_posts', 'wpb_custom_query' );
