45e08192be50578ecbff3a2d31587adcb60316e7
kilianso <hello@kilianso.com>
2021-06-07 16:53:31 +0200

    update form

--- wp-content/themes/goscho/footer.php
+++ wp-content/themes/goscho/footer.php
@@ -42,9 +42,6 @@
 					</strong>
 				</p>
 				<p>
-					Türöffnung 30 Minuten vor Konzertbeginn
-				</p>
-				<p>
 				<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2724.1334945742688!2d7.468995951831092!3d46.93941534171666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478e377b0e42dc5f%3A0x1a5f1e78b501bdc8!2sGoscho!5e0!3m2!1sde!2sch!4v1569567965674!5m2!1sde!2sch' width='600' height='450' frameborder='0' style='border:0;' allowfullscreen=''></iframe></div>
 				</p>
 			</div>
