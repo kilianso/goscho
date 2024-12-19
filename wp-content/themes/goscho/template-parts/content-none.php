82e17ca0bdf947bde738ad1cb052a83d2a0a8063
Kilian So <hello@kilianso.com>
2024-12-19 14:04:51 +0100

    better impressions page

--- wp-content/themes/goscho/template-parts/content-none.php
+++ wp-content/themes/goscho/template-parts/content-none.php
@@ -11,40 +11,21 @@
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'goscho' ); ?></h1>
+		<h1 class="page-title"><?php esc_html_e( 'Keine Events zurzeit.', 'goscho' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
 		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) :
-
-			printf(
-				'<p>' . wp_kses(
-					/* translators: 1: link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'goscho' ),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
-
+		if ( is_home() ) :
+		?>
+			<p><?php esc_html_e( 'Im Moment finden keine Events im Goscho statt.', 'goscho' ); ?></p>
+		<?php
 		elseif ( is_search() ) :
 			?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'goscho' ); ?></p>
+			<p><?php esc_html_e( 'Nichts gefunden. Bitte versuchen Sie es noch einmal mit anderen Suchbegriffen.', 'goscho' ); ?></p>
 			<?php
 			get_search_form();
-
-		else :
-			?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'goscho' ); ?></p>
-			<?php
-			get_search_form();
-
 		endif;
 		?>
 	</div><!-- .page-content -->
